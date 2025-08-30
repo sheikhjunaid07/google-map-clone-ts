2d279834448cd5f8a0e81d1062a1558d4cdb0099
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2025-08-30 19:24:25 +0530

    change marker code

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,53 @@
+// import "leaflet/dist/leaflet.css";
+// import type { Place } from "../api/Place";
+// import type { Map as LeafletMap } from "leaflet";
+// import { useEffect, useRef } from "react";
+// import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+// interface MapProps {
+//   place: Place | null;
+// }
+
+// export default function Map({ place }: MapProps) {
+//   const mapRef = useRef<LeafletMap | null>(null);
+
+//   useEffect(() => {
+//     if (mapRef.current && place) {
+//       mapRef.current.flyTo([place.latitude, place.longitude]);
+//     }
+//   }, [place]);
+
+//   return (
+//     <MapContainer
+//       ref={mapRef}
+//       center={[22.7, 75.8]}
+//       zoom={12}
+//       scrollWheelZoom
+//       className="h-full"
+//     >
+//       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+//       {place && <Marker position={[place.latitude, place.longitude]} />}
+//     </MapContainer>
+//   );
+// }
+
 import "leaflet/dist/leaflet.css";
 import type { Place } from "../api/Place";
 import type { Map as LeafletMap } from "leaflet";
 import { useEffect, useRef } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet's default icon paths for marker images
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 interface MapProps {
   place: Place | null;
